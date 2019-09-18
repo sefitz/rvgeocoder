83d767f95a3aa7fa901287994cace588ea9a2dcd
Sefi <sefi.itzkovich@gmail.com>
2019-09-18 16:39:50 +0300

    upload.sh | fix minor bug in file name (#5)

--- upload.sh
+++ upload.sh
@@ -3,7 +3,9 @@
 # you must specify version to make sure uploading correct version
 if [[ -z "$1" ]]; then
   echo "Usage: $(basename $0) version"
+  exit 1
 fi
 
-twine upload dist/rvgeocoder-${version}
+version=$1
+twine upload dist/rvgeocoder-${version}*
 
